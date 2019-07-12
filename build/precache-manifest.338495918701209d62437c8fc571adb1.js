d64c3a3d6e11f882acc0c44d979b57e9931c6be3
Nick Bueltel <nickbueltel@gmail.com>
2019-07-12 13:35:00 -0500

    move file structure

---
+++ build/precache-manifest.338495918701209d62437c8fc571adb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876841ffaf18fc6585a485eee7777fa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0a9caa2414c7c96d04",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "be4e788a073ad286ea09",
+    "url": "/static/js/2.6148393f.chunk.js"
+  },
+  {
+    "revision": "9a0a9caa2414c7c96d04",
+    "url": "/static/js/main.ab428016.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
