55f38506244030be52333b2a06f485267592d899
Nick Bueltel <nickbueltel@gmail.com>
2019-07-15 11:02:10 -0500

    cors

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const cors = require('cors')
 require('dotenv').config();
 require('./config/db');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
