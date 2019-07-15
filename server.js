609cb4c7760c1d64f23d4d5e2ecd9460ca6b48eb
Nick Bueltel <nickbueltel@gmail.com>
2019-07-15 10:56:35 -0500

    cors

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const app = express();
 const cors = require('cors')
-
 require('dotenv').config();
 require('./config/db');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
